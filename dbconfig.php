9310435abd10d65cfbccc9c6c7966548ee54b030
pandapc <rohitpanda@yopmail.com>
2014-10-26 08:10:08 -0700

    Init

---
+++ dbconfig.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("dbHOST", "localhost");     // The host you want to connect to.
+define("dbUSER", "panda");    // The database username. 
+define("dbPASSWORD", "pwd");    // The database password. 
+define("dbDATABASE", "pandabase");    // The database name.
+?>
